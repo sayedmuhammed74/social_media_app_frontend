f59ffd456f47b9af0fe569e71c2e40700342c7c7
unknown <sayedmohamed123.sm74@gmail.com>
2024-09-27 10:13:21 +0300

    last

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,5 +1,3 @@
-// import { useParams } from 'react-router-dom';
-
 import { faShare } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useSelector } from 'react-redux';
@@ -9,8 +7,8 @@ const ProfilePage = () => {
   const { user } = useSelector((state) => state.user);
   return (
     <>
-      <section className="my-4">
-        <div className="container mx-auto p-5 shadow-md rounded-md bg-white">
+      <section className="my-5">
+        <div className="container w-[90%] mx-auto p-5 shadow-md rounded-md bg-white">
           <div className="h-[300px]">
             <img
               className="h-full object-cover w-full"
@@ -31,14 +29,11 @@ const ProfilePage = () => {
           <div>
             <div className="container mx-auto px-8 flex flex-col text-gray-500">
               <h2 className="font-bold text-2xl mb-2 text-slate-800">
-                {user?.fullname}
+                {user?.firstname} {user?.lastname}
               </h2>
               <span className="ml-2">Cairo, Egypt</span>
               <span className="ml-2">@{user?.slug}</span>
               <div className="flex gap-3 my-2 font-medium text-lg">
-                <button className="px-4 py-1 rounded-sm hover:opacity-95 hover:scale-95 transition-all duration-75 bg-gray-400 text-slate-800">
-                  Message
-                </button>
                 <button className="flex px-5 rounded-sm items-center gap-2 hover:opacity-95 hover:scale-95 transition-all duration-75 text-white bg-primary">
                   <FontAwesomeIcon icon={faShare} style={{ color: 'white' }} />
                   Share Profile
@@ -47,9 +42,7 @@ const ProfilePage = () => {
             </div>
           </div>
         </div>
-      </section>
-      <section>
-        <div className="container mx-auto p-5 shadow-md rounded-md bg-white">
+        <div className="container w-[90%] mt-5 mx-auto p-5 shadow-md rounded-md bg-white">
           <Posts userId={user?._id} />
         </div>
       </section>
