cb4ef944febecd5b4a530c01a6d2a18d83a341f0
unknown <sayedmohamed123.sm74@gmail.com>
2024-09-30 12:24:58 +0300

    likes & comments

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
-// export const url = 'https://social-media-app-backend-hsgm.onrender.com';
-export const url = 'http://localhost:8000';
+export const url = 'https://social-media-app-backend-hsgm.onrender.com';
+// export const url = 'http://localhost:8000';
